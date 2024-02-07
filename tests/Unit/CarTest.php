d406717eb8b5cdc999ccb34ed4c327205d85052c
Antonio Pavlov <apavlov@weband.bg>
2024-02-07 11:03:26 +0200

    unit test example

---
+++ tests/Unit/CarTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class CarTest extends TestCase
+{
+
+    public function test_my_first_test()
+    {
+        $query = 'Antonio';
+        $this->assertEquals('Antonio', $query);
+    }
+
+}
